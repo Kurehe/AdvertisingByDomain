ec0ae481f0185e414eb90e78420577dc42388470
Kurehe <artem.kazak002@gmail.com>
2025-03-16 02:14:59 +0300

    Набросок в стиле трех слойного шаблона проектирования

---
+++ AdvertisingByDomain/Controllers/AdvertisingLocationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisingByDomain.Controllers
+{
+    [Route("Advertising")]
+    [ApiController]
+    public class AdvertisingLocationController : ControllerBase
+    {
+        private readonly ILogger<AdvertisingLocationController> logger;
+
+        public AdvertisingLocationController(ILogger<AdvertisingLocationController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPut("UploadFile")]
+        public async Task<IActionResult> UploadFile()
+        {
+            await Task.Delay(10);
+            logger.Log(LogLevel.Information, " > UploadFile");
+            return Ok();
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetPlatformByLocation([FromQuery] string[] ArrayLocation)
+        {
+            await Task.Delay(10);
+            logger.Log(LogLevel.Information, " > GetPlatformByLocation");
+            string mes = "";
+            foreach (var item in ArrayLocation)
+            {
+                mes += $"\t- >  {item}\n";
+            }
+            logger.Log(LogLevel.Information, mes);
+            return Ok();
+        }
+    }
+}
--- AdvertisingByDomain/Program.cs
+++ AdvertisingByDomain/Program.cs
@@ -1,3 +1,6 @@
+using BusinessLogicLayer;
+using DataAccessLayer;
+
 namespace AdvertisingByDomain
 {
     public class Program
@@ -6,26 +9,30 @@ namespace AdvertisingByDomain
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var services = builder.Services;
+
             // Add services to the container.
 
-            builder.Services.AddControllers();
-                       
+            services.AddControllers();
+
+            services.AddSingleton<DataDomainsRepository>(); // dal
+            services.AddTransient<DomainServices>();        // bll
+
+            services.AddSwaggerGen();           
             
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
+            
             app.MapControllers();
-
             app.Run();
         }
     }
